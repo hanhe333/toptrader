1f337d7592c025ec48bdbe122d5b3fb071ca1595
Han He <hanhe@Hans-MacBook-Air.local>
2013-01-24 19:04:09 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Toptrader::Application.config.secret_token = '9b80b1da11510180934c8ab8ef17a4fa77279c5324a570aeba83cef9ad985d29864781f900fac903732e7d8bb20235d4bccc0b5f61a6e6cd1109abc0c1173955'
